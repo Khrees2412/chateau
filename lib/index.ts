bcdd291a30e5537c5125db367a51a602df1c1218
Christian Ndu <christiannduh@gmail.com>
2023-10-23 15:57:04 +0100

    testing fix

--- lib/index.ts
+++ lib/index.ts
@@ -1,19 +1,36 @@
 import express, {Application, Request, Response} from "express";
+import http from "http"
 import dotenv from "dotenv";
 import {HTTPStatusCode} from "./utils/misc";
 import roomRouter from "./routes/room";
 import logger from "./utils/logger";
 import path from "path";
 import authRouter from "./routes/auth";
-import {createServer} from "http";
 import morganMiddleware from "./middleware/morgan";
-import startSocketConn from "./server";
 import startMetricsCollection from "./middleware/metrics";
+import SocketIO from "socket.io";
+import connection from "./socket";
 
 const app: Application = express();
-startSocketConn(app)
+const dev = process.env.NODE_ENV !== "production";
+
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "*:*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+    },
+    cookie: {
+        name: "chateau-socket-cookie",
+        httpOnly: true,
+        maxAge: 14 * 24 * 60 * 60 * 1000, // expires in 14 days
+        domain: dev ? "localhost" : "chateau-r0dz.onrender.com",
+        secure: dev ? false : true,
+    },
+    transports: ["polling", "websocket"],
+});
+connection(io);
 startMetricsCollection(app)
-const httpServer = createServer(app);
 
 dotenv.config();
 
@@ -37,17 +54,8 @@ app.get("/", async (req: Request, res: Response): Promise<Response> => {
 
 
 const port = process.env.PORT || 5001;
-httpServer.listen(port, (): void => {
+app.listen(port, (): void => {
     logger.info(`Connected successfully on port ${port}`);
 });
 
 // Graceful shutdown
-process.on("SIGTERM", () => {
-    httpServer.close((err) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        process.exit(0);
-    });
-});
